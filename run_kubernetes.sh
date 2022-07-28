a56e433b5f467ad9e13ea2f8af61df0609382049
MsOluwademilade <oyekanmidemilade2@gmail.com>
2022-07-28 13:11:18 +0000

    edited kubernetes run script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=oluwademilade/project-4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run project-4 --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pods/project-4 8080:80
